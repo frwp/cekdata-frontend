3fc349365e86ce7f824b7647043ecf99894c8bf7
Fransiskus Rian Wardana Putra <f.rianwardanaputra@gmail.com>
2021-07-16 11:27:38 +0700

    changed image render method using gatsby plugins

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,18 +2,21 @@ import '../styles/global.css';
 import React from 'react'
 import Header from './Header'
 import { Link } from 'gatsby';
-import ugmLogo from '../images/Logo Tengah Stack-Up 1.png';
-import kknLogo from '../images/logo_bersamaayah.png'
+// import ugmLogo from '../images/Logo Tengah Stack-Up 1.png';
+// import kknLogo from '../images/logo_bersamaayah.png'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons'
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function Layout({ children, location }) {
   return (
     <div className="container">
       <nav className="layout">
         {/* <Link style={{ margin: '1em' }} to="/test">Test</Link> */}
-        <img src={kknLogo} style={{ height: '81px', marginInline: '0.5em' }} alt="Logo KKN Bersama Ayah" />
-        <img src={ugmLogo} style={{ width: '80px', marginInline: '0.5em' }} alt="Logo UGM" />
+        {/* <img src={kknLogo} style={{ height: '81px', marginInline: '0.5em' }} alt="Logo KKN Bersama Ayah" /> */}
+        {/* <img src={ugmLogo} style={{ width: '80px', marginInline: '0.5em' }} alt="Logo UGM" /> */}
+        <StaticImage style={{ marginInline: '0.5em' }} src='../images/logo_bersamaayah.png' height={81} alt="Logo KKN PPM UGM Bersama Ayah" placeholder="blurred" />
+        <StaticImage style={{ marginInline: '0.5em' }} src='../images/Logo Tengah Stack-Up 1.png' width={80} alt="Logo UGM" placeholder="blurred" />
         {location.pathname !== '/' && <Link style={{ margin: '1em', marginRight: 'auto' }} to="/"><FontAwesomeIcon icon={faAngleDoubleLeft} size="3x" color="#5B4434" /></Link>}
       </nav>
       <div className={location.pathname === '/' ? 'header-right' : ''}>
